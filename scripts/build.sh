9fe20152857c02fef7c8f3d118248e9c2599ef51
David Wirth <dwirth@cloudflare.com>
2025-10-29 12:33:14 -0500

    chore: update config for non monorepo

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-root=$1
 
 pnpm exec babel ./src --out-dir ./es --config-file ./babel/babel.es.config.js -x ".js,.ts,.tsx" --copy-files --no-copy-ignored
 pnpm exec babel ./src --out-dir ./lib --config-file ./babel/babel.lib.config.js -x ".js,.ts,.tsx" --copy-files --no-copy-ignored
-
-
